4a284cede638cfc1a5f255f193c3ea64488b7435
GarikGelios <garikgelios@gmail.com>
2020-05-03 17:45:14 +0300

    lesson 9

--- myapp/src/Field.vue
+++ myapp/src/Field.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <input type="text" v-model="mess" />
+    <h3>{{ msg }}</h3>
+    <input type="text" :value="msg" @input="changeMess" />
     <p>{{ mess }}</p>
   </div>
 </template>
 
 <script>
 export default {
+    props: ['msg'],
     data () {
         return {
             mess: ''
+        }
+    },
+    methods: {
+        changeMess (event) {
+            this.mess = event.target.value; 
+            this.$emit('messChange', this.mess)
         }
     }
 };
